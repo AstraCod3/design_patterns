68e82cfc989f092b29331e1c296e00fa493889d2
millenium <lukeskywalker0@gmail.com>
2026-07-16 18:12:18 +0200

    modify observer

--- behavioral_pattern/observer/observer/observer.hpp
+++ behavioral_pattern/observer/observer/observer.hpp
@@ -132,38 +132,3 @@ class Observer : public IObserver {
   static int static_number_;
   int number_;
 };
-
-int Observer::static_number_ = 0;
-
-int main() {
-  Subject *subject = new Subject;
-  Observer *observer1 = new Observer(*subject);
-  Observer *observer2 = new Observer(*subject);
-  Observer *observer3 = new Observer(*subject);
-  Observer *observer4;
-  Observer *observer5;
-
-  subject->CreateMessage("Hello World! :D");
-  observer3->RemoveMeFromTheList();
-
-  subject->CreateMessage("The weather is hot today! :p");
-  observer4 = new Observer(*subject);
-
-  observer2->RemoveMeFromTheList();
-  observer5 = new Observer(*subject);
-
-  subject->CreateMessage("My new car is great! ;)");
-  observer5->RemoveMeFromTheList();
-
-  observer4->RemoveMeFromTheList();
-  observer1->RemoveMeFromTheList();
-
-  delete observer5;
-  delete observer4;
-  delete observer3;
-  delete observer2;
-  delete observer1;
-  delete subject;
-
-  return 0;
-}
