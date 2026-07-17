c20ee073ae34b0db1cfd43dba94783fccb6779ab
unknown <lukeusethecode>
2026-07-17 07:54:22 +0100

    update abstract factory

--- creational_pattern/factory_method/src/factory_method.cpp
+++ creational_pattern/factory_method/src/factory_method.cpp
@@ -5,14 +5,15 @@
  * Copyright   : 
  * Description :
  *
- * Factory method is a creational design pattern  
- *  which solves the problem of creating product objects without specifying their concrete classes.
- *   
+ * Factory method is a creational design pattern
+ *  which solves the problem of creating product objects
+ *  without specifying their concrete classes.
+ *
  *   The Factory Method defines a method, 
- *    which should be used for creating objects instead of using a direct constructor call (new operator).
+ *    which should be used for creating objects
+ *    instead of using a direct constructor call (new operator).
  *   Subclasses can override this method to change the class of objects that will be created.
- *   
- *============================================================================ 
+ *
  */
  
 #include <iostream>
